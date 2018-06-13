9a8295e237c80bb1391bfa1c71ba971d44edc5c4
Rian Stockbower <rstockbower@cimpress.com>
2018-06-13 09:37:05 -0400

    Substring search

---
+++ PerfTesting/PerfTesting/Runner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess;
+using PerfTesting.Strings;
+
+namespace PerfTesting
+{
+    public class Runner
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunnerCore.Run(BenchmarkConverter.TypeToBenchmarks(typeof(Substrings)), t => InProcessToolchain.Instance);
+        }
+    }
+}
