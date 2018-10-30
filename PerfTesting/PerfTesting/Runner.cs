50e68edb94c7c80dd1f01c65f728f207bc8f4614
Rian Stockbower <rstockbower@carestack.com>
2018-10-30 10:30:44 -0400

    Regex tests

--- PerfTesting/PerfTesting/Runner.cs
+++ PerfTesting/PerfTesting/Runner.cs
@@ -10,11 +10,11 @@ namespace PerfTesting
     {
         static void Main(string[] args)
         {
-            BenchmarkRunnerCore.Run(BenchmarkConverter.TypeToBenchmarks(typeof(ExtensionMethods)), t => InProcessToolchain.Instance);
-            BenchmarkRunnerCore.Run(BenchmarkConverter.TypeToBenchmarks(typeof(SubstringTests)), t => InProcessToolchain.Instance);
+            //BenchmarkRunnerCore.Run(BenchmarkConverter.TypeToBenchmarks(typeof(ExtensionMethods)), t => InProcessToolchain.Instance);
+            //BenchmarkRunnerCore.Run(BenchmarkConverter.TypeToBenchmarks(typeof(SubstringTests)), t => InProcessToolchain.Instance);
             BenchmarkRunnerCore.Run(BenchmarkConverter.TypeToBenchmarks(typeof(RegexTests)), t => InProcessToolchain.Instance);
-            BenchmarkRunnerCore.Run(BenchmarkConverter.TypeToBenchmarks(typeof(StringBuilderTests)), t => InProcessToolchain.Instance);
-            BenchmarkRunnerCore.Run(BenchmarkConverter.TypeToBenchmarks(typeof(SynchronizationTests)), t => InProcessToolchain.Instance);
+            //BenchmarkRunnerCore.Run(BenchmarkConverter.TypeToBenchmarks(typeof(StringBuilderTests)), t => InProcessToolchain.Instance);
+            //BenchmarkRunnerCore.Run(BenchmarkConverter.TypeToBenchmarks(typeof(SynchronizationTests)), t => InProcessToolchain.Instance);
         }
     }
 }
